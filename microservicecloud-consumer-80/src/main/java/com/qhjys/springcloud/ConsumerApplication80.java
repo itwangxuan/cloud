d95e3c75db4528abb223c1766ea6e2d8969b24d2
王轩 <952904451@qq.com>
2019-03-07 10:44:40 +0800

    fix

--- microservicecloud-consumer-80/src/main/java/com/qhjys/springcloud/ConsumerApplication80.java
+++ microservicecloud-consumer-80/src/main/java/com/qhjys/springcloud/ConsumerApplication80.java
@@ -2,9 +2,11 @@ package com.qhjys.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient  //服务发现
 public class ConsumerApplication80 {
 
     public static void main(String[] args) {
