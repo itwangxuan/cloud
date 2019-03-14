4fc31e6381bd20aef393e89fc8846dfbd05d6abd
王轩 <952904451@qq.com>
2019-03-14 11:11:36 +0800

    fix

--- microservicecloud-consumer-hystrix-dashboard/src/main/java/com/qhjys/springcloud/DeptConsumer_Dashboard.java
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/qhjys/springcloud/DeptConsumer_Dashboard.java
@@ -4,19 +4,28 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EnableHystrix
 @EnableHystrixDashboard
 public class DeptConsumer_Dashboard {
+
+    /**
+     * 豪猪界面可以展示  但是连接失败
+     *
+     * @param args
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_Dashboard.class, args);
     }
 
-    //版本如果是2.0则需要添加 ServletRegistrationBean 因为springboot的默认路径不是
-    // "/hystrix.stream"，只要在自己的项目里配置上下面的servlet就可以了
+    //版本如果是2.0则需要添加 ServletRegistrationBean 因为springboot的默认路径不是"/hystrix.stream"，只要在自己的项目里配置上下面的servlet就可以了
     @Bean
     public ServletRegistrationBean getServlet() {
         HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
